d637e0e5795aa675847c907d75fc71b40cb4947b
Thiago van Dieten <thiago@tvdieten.com>
2024-10-13 21:09:30 +0200

    add counting words

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+def count_words_in_file(content):
+    contents_in_list = content.split()
+    return len(contents_in_list)
+
 def main():
     with open('books/frankenstein.txt', 'r') as file:
         file_contents = file.read()
         print(file_contents)
+
+        print(count_words_in_file(file_contents))
 
 main()
